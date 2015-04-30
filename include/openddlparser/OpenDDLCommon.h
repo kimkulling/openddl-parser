d25fa76d5b26768e2e58f377f9f3964c6c702377
Kim Kulling <kim.kulling@googlemail.com>
2015-04-30 19:48:15 +0200

    add 64bit types for gcc and clang.
    Signed-off-by: Kim Kulling <kim.kulling@googlemail.com>

--- include/openddlparser/OpenDDLCommon.h
+++ include/openddlparser/OpenDDLCommon.h
@@ -29,6 +29,9 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <string>
 
 #include <string.h>
+#ifndef _WIN32
+#  include <inttypes.h>
+#endif 
 
 #ifdef _MSC_VER
 #   define TAG_DLL_EXPORT __declspec(dllexport)
@@ -67,14 +70,17 @@ struct DataArrayList;
 typedef char           int8;
 typedef short          int16;
 typedef int            int32;
-//typedef long           int64;
 typedef unsigned char  uint8;
 typedef unsigned short uint16;
 typedef unsigned int   uint32;
-//typedef unsigned long  uint64;
 
-typedef __int64					int64;
-typedef unsigned __int64		uint64;
+#ifdef _WIN32
+typedef __int64           int64;
+typedef unsigned __int64  uint64;
+#else
+typedef int64_t           int64;
+typedef uint64_t          uint64;
+#endif
 
 enum NameType {
     GlobalName,
