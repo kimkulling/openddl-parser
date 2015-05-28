77751c8a5c11074cc79c5db293e6bc045aed16f7
Kim Kulling <kim.kulling@googlemail.com>
2015-05-28 08:42:18 +0200

    Add type size checks.

--- test/OpenDDLCommonTest.cpp
+++ test/OpenDDLCommonTest.cpp
@@ -54,6 +54,18 @@ public:
     }
 };
 
+TEST_F( OpenDDLCommonTest, typeSizesTest ) {
+    EXPECT_EQ( 1, sizeof( int8 ) );
+    EXPECT_EQ( 2, sizeof( int16 ) );
+    EXPECT_EQ( 4, sizeof( int32 ) );
+    EXPECT_EQ( 8, sizeof( int64 ) );
+
+    EXPECT_EQ( 1, sizeof( uint8 ) );
+    EXPECT_EQ( 2, sizeof( uint16 ) );
+    EXPECT_EQ( 4, sizeof( uint32 ) );
+    EXPECT_EQ( 8, sizeof( uint64 ) );
+}
+
 TEST_F( OpenDDLCommonTest, createNameTest ) {
     bool ok( true );
     Name *name( nullptr );
